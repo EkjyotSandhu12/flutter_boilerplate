0a0eddd6e6f7c716259852911c6eb6637d18e8b7
Ekjyot-Occult <ekjyot.sandhu@weboccult.com>
2024-03-07 13:04:12 +0530

    first commit

---
+++ lib/common/constants/assets_constants.dart
@@ -0,0 +1,21 @@
+
+
+
+class ImageConstants{
+
+  String path1 = 'assets/';
+  String path2 = 'assets/';
+
+  ///==> IMAGES
+
+}
+
+
+class SVGConstants{
+
+  String path1 = 'assets/';
+  String path2 = 'assets/';
+
+///==> SVG's
+
+}
